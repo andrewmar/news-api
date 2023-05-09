74efc589a22cf68cf47177f0da384c97379e0f3e
Andrii Martseniuk <martsukandrew@gmail.com>
2023-05-09 14:27:08 +0100

    add get/api/topics endpoint

---
+++ __tests__/topics.tests.js
@@ -0,0 +1,33 @@
+const request = require("supertest");
+const app = require("../app.js");
+const connection = require("../db/connection.js");
+const seed = require("../db/seeds/seed.js");
+const testData = require("../db/data/test-data/index.js");
+
+beforeEach(() => {
+  return seed(testData);
+});
+
+afterAll(() => {
+  connection.end();
+});
+
+describe("GET api/topics", () => {
+  test("GET - status: 200 - responds with an array of topic objects ", () => {
+    return request(app)
+      .get("/api/topics")
+      .expect(200)
+      .then((response) => {
+        const { topics } = response.body;
+
+        expect(topics.length).toBe(3);
+        topics.forEach((topic) => {
+          expect(typeof topic.slug).toBe("string");
+          expect(typeof topic.description).toBe("string");
+        });
+      });
+  });
+  test("GET - status: 404 - when provided non-existen endpoint  ", () => {
+    return request(app).get("/api/topicss").expect(404);
+  });
+});
